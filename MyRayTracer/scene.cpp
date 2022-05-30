59d9db26e50756df8bfb46c1f2e956a925405447
Afonso <afonso.diamantino2@gmail.com>
2022-05-30 14:31:54 +0100

    Kay & Kajiya, solves #2

--- MyRayTracer/scene.cpp
+++ MyRayTracer/scene.cpp
@@ -150,9 +150,96 @@ AABB aaBox::GetBoundingBox() {
 
 bool aaBox::intercepts(Ray& ray, float& t)
 {
-	//PUT HERE YOUR CODE
-		return (false);
+	double ox = ray.origin.x;
+	double oy = ray.origin.y;
+	double oz = ray.origin.z;
+
+	double dx = ray.direction.x;
+	double dy = ray.origin.y;
+	double dz = ray.origin.z;
+
+	double tx_min, ty_min, tz_min;
+	double tx_max, ty_max, tz_max;
+
+	/* CALCULATE INTERSECTIONS */
+	double a = 1.0 / dx;
+	if (a >= 0) {
+		tx_min = (min.x - ox) * a;
+		tx_max = (max.x - ox) * a;
+	}
+	else { 
+		tx_min = (max.x - ox) * a;
+		tx_max = (min.x - ox) * a;
+	}
+
+	double b = 1.0 / dy;
+	if (b >= 0) {
+		ty_min = (min.y - oy) * b;
+		ty_max = (max.y - oy) * b;
+	}
+	else {
+		ty_min = (max.y - oy) * b;
+		ty_max = (min.y - oy) * b;
+	}
+
+	double c = 1.0 / dz;
+	if (c >= 0) {
+		tz_min = (min.z - oz) * c;
+		tz_max = (max.z - oz) * c;
+	}
+	else {
+		tz_min = (max.z - oz) * c;
+		tz_max = (min.z - oz) * c;
+	}
+
+	float tE, tL; // Entering and leaving t values
+	Vector face_in, face_out; // Normals
+
+	/* FIND LARGEST ENTERING T VALUE*/
+	if (tx_min > ty_min) {
+		tE = tx_min;
+		face_in = (a >= 0.0) ? Vector(-1, 0, 0) : Vector(1, 0, 0);
+	}
+	else {
+		tE = ty_min;
+		face_in = (b >= 0.0) ? Vector(0, -1, 0) : Vector(0, 1, 0);
+	}
+	if (tz_min > tE) {
+		tE = tz_min;
+		face_in = (c >= 0) ? Vector(0, 0, -1) : Vector(0, 0, 1);
+	}
+
+	/* FIND SMALLEST LEAVING T VALUE*/
+	if (tx_max < ty_max) {
+		tL = tx_max;
+		face_out = (a >= 0.0) ? Vector(1, 0, 0) : Vector(-1, 0, 0);
+	}
+	else {
+		tL = ty_max;
+		face_out = (b >= 0.0) ? Vector(0, 1, 0) : Vector(0, -1, 0);
+	}
+	if (tz_max < tL) {
+		tL = tz_max;
+		face_out = (c >= 0) ? Vector(0, 0, 1) : Vector(0, 0, -1);
+	}
+
+	/* Condition to HIT */
+	if (tE < tL && tL > 0) {
+		if (tE > 0) {
+		// ray hits outside surface
+			t = tE;
+			Normal = face_in;
+		}
+		else {
+		// ray hits inside surface
+			t = tL;
+			Normal = face_out;
+		}
+		return (true);
+	}
+	else return (false);
 }
+
 
 Vector aaBox::getNormal(Vector point)
 {
